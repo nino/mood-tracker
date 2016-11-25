c262c68249556607ba14cffaa43c6a4e9aad9544
Nino Annighöfer <nino@ninoan.com>
2016-11-25 10:20:35 +0000

    Add ActivityIndicator test

--- src/ActivityIndicator/ActivityIndicator.test.js
+++ src/ActivityIndicator/ActivityIndicator.test.js
@@ -4,30 +4,31 @@ import {shallow, mount} from 'enzyme'
 import {expect} from 'chai'
 
 describe('ActivityIndicator', () => {
-	it('mounts without crashing', () => {
-		const component1 = mount(<ActivityIndicator activities={null} />)
-		const component2 = mount(
-			<ActivityIndicator activities={[
-				{ name: 'Syncing data', id: 1 },
-				{ name: 'Syncing more data', id: 2 }
-			]} />
-		)
+    it('mounts without crashing', () => {
+        const activitiesArray = [
+            { name: 'Syncing data', id: 1 },
+            { name: 'Syncing more data', id: 2 }
+        ]
+        const component1 = mount(<ActivityIndicator activities={null} />)
+        const component2 = mount(
+            <ActivityIndicator activities={activitiesArray} />
+        )
 
-		expect(component1).to.be.ok
-		expect(component2).to.be.ok
-	})
+        expect(component1).to.be.ok
+        expect(component2).to.be.ok
+    })
 
-	it('renders a div with id "no-activities" if no activities are present', () => {
-		const component = shallow(<ActivityIndicator />)
-		expect(component.find('#no-activities')).to.have.length(1)
-	})
+    it('renders a div with id "no-activities" if no activities are present', () => {
+        const component = shallow(<ActivityIndicator />)
+        expect(component.find('.activity')).to.have.length(0)
+    })
 
-	it('renders all activities if activities are present', () => {
-		const component = shallow(<ActivityIndicator activities={[
-			{name: 'syncing data', id: 1},
-			{name: 'another thing', id: 2}
-		]} /> )
-		expect(component.find('another thing')).to.have.length(1)
-	})
-
+    it('renders all activities if activities are present', () => {
+        const activitiesArray = [
+            { name: 'Syncing data', id: 1 },
+            { name: 'Syncing more data', id: 2 }
+        ]
+        const component = shallow(<ActivityIndicator activities={activitiesArray} />)
+        expect(component.find('.activity')).to.have.length(2)
+    })
 })
