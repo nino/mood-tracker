386d46ffc31082071923bbccb217df3b4994630e
Nino Annighöfer <nino@ninoan.com>
2017-01-07 14:54:38 +0100

    Define prop-type-shapes for app sub-states

--- src/types.js
+++ src/types.js
@@ -25,3 +25,42 @@ export const metricShape = PropTypes.shape({
   lastModified: PropTypes.number,
   entries: PropTypes.arrayOf(entryShape).isRequired,
 });
+
+export const modalActionShape = PropTypes.shape({
+  label: PropTypes.string.isRequired,
+  action: PropTypes.shape({ type: PropTypes.string }),
+});
+
+export const modalShape = PropTypes.shape({
+  title: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  actions: PropTypes.shape({
+    confirm: modalActionShape,
+    cancel: modalActionShape,
+  }),
+});
+
+export const stateShapes = {
+  metrics: PropTypes.shape({
+    isSyncing: PropTypes.bool.isRequired,
+    isSynced: PropTypes.bool.isRequired,
+    lastSynced: PropTypes.number,
+    items: PropTypes.arrayOf(metricShape),
+    hasError: PropTypes.shape({ error: PropTypes.string }),
+  }),
+  authentication: PropTypes.shape({
+    isAuthenticated: PropTypes.bool.isRequired,
+    isAuthenticating: PropTypes.bool.isRequired,
+    hasError: PropTypes.shape({ error: PropTypes.string }),
+    accessToken: PropTypes.string,
+    lastAuthenticated: PropTypes.number,
+  }),
+  modals: PropTypes.arrayOf(modalShape),
+  settings: PropTypes.shape({
+    editedMetric: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      props: propsShape,
+    }),
+    isModified: PropTypes.bool.isRequired,
+  }),
+};
