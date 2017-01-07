b306cf2482388d8cce55f89f01b0d33aafc9f95e
Nino Annighöfer <nino@ninoan.com>
2017-01-07 11:13:30 +0100

    Rewrite color-groups settings UI
    The color-groups components are now unaware of which metric they are
    editing or what form they are part of.

--- src/Settings/ColorGroupsSettings.js
+++ src/Settings/ColorGroupsSettings.js
@@ -1,125 +1,93 @@
 import React from 'react';
-import './ColorGroupsSettings.css';
-import {Input, Button} from 'semantic-ui-react';
+import Button from '../components/Button';
+import { colorGroupShape } from '../types';
 
-const SingleColorGroupSettings = ({colorGroup, onChange, formId, editing, onDelete}) => {
-  if (!colorGroup) {
-    return (<div>Error</div>);
+export const SingleColorGroupSettings = ({ colorGroup, onUpdate, editing, onDelete }) => (
+  <div className={`single-color-group-${editing ? 'editing' : 'not-editing'}`}>
+    From
+    <input
+      onChange={e => onUpdate({ minValue: e.target.value })}
+      name="minValue"
+      className="color-group-minValue-field"
+      value={colorGroup.minValue}
+    />
+    to
+    <input
+      onChange={e => onUpdate({ maxValue: e.target.value })}
+      name="maxValue"
+      className="color-group-maxValue-field"
+      value={colorGroup.maxValue}
+    />
+    use
+    <input
+      onChange={e => onUpdate({ color: e.target.value })}
+      name="color"
+      className="color-group-color-field"
+      value={colorGroup.color}
+    />
+    { editing ? (
+      <Button onClick={onDelete} className="delete-color-group-button">X</Button>
+      ) : (null)
+    }
+  </div>
+);
+
+SingleColorGroupSettings.propTypes = {
+  colorGroup: colorGroupShape.isRequired,
+  onUpdate: React.PropTypes.func.isRequired,
+  editing: React.PropTypes.bool,
+  onDelete: React.PropTypes.func.isRequired,
+};
+
+const ColorGroupsSettings = ({ colorGroups, onUpdate, editing }) => {
+  function handleChange(index, updatedField) {
+    onUpdate({
+      colorGroups: colorGroups.slice(0, index).concat(
+        { ...colorGroups[index], ...updatedField },
+        colorGroups.slice(index + 1, colorGroups.length),
+      ),
+    });
+  }
+
+  function handleAddColorGroup() {
+    onUpdate({
+      colorGroups: colorGroups.concat({
+        minValue: null,
+        maxValue: null,
+        color: '',
+      }),
+    });
+  }
+
+  function handleDeleteColorGroup(idx) {
+    onUpdate({
+      colorGroups: colorGroups.slice(0, idx).concat(colorGroups.slice(idx + 1, colorGroups.length)),
+    });
   }
 
   return (
-    <div className='single-color-group-settings'>
-      From 
-      <Input size='mini' type='text' onChange={onChange} name='minValue'
-        disabled={!editing}
-        value={colorGroup.minValue}
-        className='color-group-minValue-field'/> 
-      to 
-      <Input size='mini' type='text' onChange={onChange} name='maxValue'
-        disabled={!editing}
-        value={colorGroup.maxValue}
-        className='color-group-maxValue-field'/>, 
-      use color 
-      <Input size='mini' type='text' onChange={onChange} name='color'
-        disabled={!editing}
-        value={colorGroup.color}
-        className='color-group-color-field'/>.
+    <div className={`color-groups-settings-${editing ? 'editing' : 'not-editing'}`}>
+      {colorGroups.map((colorGroup, idx) => (
+        <SingleColorGroupSettings
+          colorGroup={colorGroup}
+          key={idx}
+          onUpdate={updatedField => handleChange(idx, updatedField)}
+          onDelete={() => handleDeleteColorGroup(idx)}
+          editing={editing}
+        />
+      ))}
       {editing ? (
-        <Button type='button' onClick={onDelete}
-          className='delete-color-group-button'
-          size='mini'>
-          Delete color group
+        <Button className="add-color-group-button" onClick={handleAddColorGroup}>
+          Add color group
         </Button>
-      ) : (<div />)}
+        ) : null}
     </div>
-  );
-}
-
-SingleColorGroupSettings.propTypes = {
-  colorGroup: React.PropTypes.object.isRequired,
-  formId: React.PropTypes.string.isRequired,
-  onChange: React.PropTypes.func.isRequired,
-  onDelete: React.PropTypes.func.isRequired,
-  editing: React.PropTypes.bool,
-};
-
-/**
- * Container for the form to edit color groups.
- */
-const ColorGroupsSettings = ({colorGroups, onAction, formId, editing}) => {
-  /**
-   * Since the `ColorGroupsSettings` component gets treated as
-   * a single form element, new color groups have to be created here,
-   * and handed upstairs to the app-state.
-   * This means the entire `colorGroups` array is the "value" 
-   * of the "form field".
-   */
-  const sendUpdate = (updatedColorGroups) => {
-    onAction('update form element', {
-      name: 'colorGroups',
-      formId: formId,
-      value: updatedColorGroups,
-    });
-  };
-
-  const handleClickNewGroup = (event) => {
-    if (!colorGroups) {
-      colorGroups = [];
-    }
-    let newColorGroups = colorGroups.concat({
-      minValue: 0,
-      maxValue: 0,
-      color: '',
-    });
-    sendUpdate(newColorGroups);
-  };
-
-  const handleDeleteGroup = (groupIndex) => {
-    let newColorGroups = colorGroups.slice(0, groupIndex)
-      .concat(colorGroups.slice(groupIndex+1, colorGroups.length));
-    sendUpdate(newColorGroups);
-  };
-
-  const handleChange = (event, groupIndex) => {
-    let updatedField = {};
-    updatedField[event.target.name] = event.target.value;
-    let updatedGroup = Object.assign(
-      {}, colorGroups[groupIndex], updatedField
-    );
-    let newColorGroups = colorGroups.slice(0, groupIndex).concat(
-      updatedGroup,
-      colorGroups.slice(groupIndex+1, colorGroups.length),
-    );
-    sendUpdate(newColorGroups);
-  };
-
-  return (
-    <div className='color-groups-settings'>
-      <div className='color-groups-settings-title'>
-        Color groups
-      </div>
-      {colorGroups && colorGroups.length > 0 ?
-          colorGroups.map((colorGroup, index) => (
-            <SingleColorGroupSettings
-              key={index} colorGroup={colorGroup}
-              formId={formId} editing={editing}
-              onDelete={() => handleDeleteGroup(index)}
-              onChange={(event) => handleChange(event, index)}/>
-          )) : (<div className='nocolorgroupsmessage'>(No color groups)</div>)}
-          {editing ? (
-            <Button type='button' className='add-color-group-button'
-              size='mini' onClick={handleClickNewGroup}>
-              Add color group
-            </Button>
-          ) : (<div />)}
-        </div>
   );
 };
 
 ColorGroupsSettings.propTypes = {
-  colorGroups: React.PropTypes.array,
-  onAction: React.PropTypes.func.isRequired,
-  formId: React.PropTypes.string.isRequired,
+  colorGroups: React.PropTypes.arrayOf(colorGroupShape).isRequired,
+  onUpdate: React.PropTypes.func.isRequired,
   editing: React.PropTypes.bool,
 };
 
--- src/Settings/ColorGroupsSettings.test.js
+++ src/Settings/ColorGroupsSettings.test.js
@@ -1,181 +1,134 @@
+/* eslint-env jest */
 import React from 'react';
-import {expect} from 'chai';
-import {shallow, mount} from 'enzyme';
-import ColorGroupsSettings from './ColorGroupsSettings';
-import SampleMetricsWithEntries from '../../test/SampleMetricsWithEntries';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import ColorGroupsSettings, { SingleColorGroupSettings } from './ColorGroupsSettings';
+import { MoodWithEntries } from '../../test/SampleMetrics';
 
-let componentMountedEditing, componentEditing, componentNotEditing;
-let cbAction, cbParams;
-const callback = (action, params) => {
-  cbAction = action;
-  cbParams = params;
-};
-
-describe('ColorGroupsSettings', () => {
-  beforeAll(() => {
-    componentMountedEditing = mount(
-      <ColorGroupsSettings
-        colorGroups={SampleMetricsWithEntries[0].colorGroups}
-        onAction={callback}
-        editing={true}
-        formId='metric-settings-form-1'/>
-    );
-    componentEditing = shallow(
-      <ColorGroupsSettings
-        colorGroups={SampleMetricsWithEntries[0].colorGroups}
-        onAction={callback}
-        editing={true}
-        formId='metric-settings-form-1'/>
-    );
-    componentNotEditing = shallow(
-      <ColorGroupsSettings
-        colorGroups={SampleMetricsWithEntries[0].colorGroups}
-        onAction={callback}
-        editing={false}
-        formId='metric-settings-form-1'/>
-    );
-  });
-
-  it('mounts without crashing', () => {
-    expect(componentMountedEditing).to.be.ok;
-  });
-
-  it('contains 4 SingleColorGroupSettings children', () => {
-    expect(componentNotEditing.find('SingleColorGroupSettings'))
-      .to.have.length(4);
-    expect(componentEditing.find('SingleColorGroupSettings'))
-      .to.have.length(4);
-  });
-
-  it('contains an "Add color group" button when editing', () => {
-    expect(componentEditing.find('Button.add-color-group-button'))
+describe('SingleColorGroupSettings', () => {
+  it('renders all text fields', () => {
+    const component = shallow(
+      <SingleColorGroupSettings colorGroup={MoodWithEntries.props.colorGroups} editing />);
+    expect(component.find('.color-group-maxValue-field'), 'must render maxValue field')
+      .to.have.length(1);
+    expect(component.find('.color-group-minValue-field'), 'must render minValue field')
+      .to.have.length(1);
+    expect(component.find('.color-group-color-field'), 'must render color field')
       .to.have.length(1);
   });
 
-  it('doesn\'t contain an "Add color group" button when not editing', () => {
-    expect(componentNotEditing.find('Button.add-color-group-button'))
-      .to.have.length(0);
+  it('renders a "delete color group" button if editing', () => {
+    const component = shallow(
+      <SingleColorGroupSettings
+        colorGroup={MoodWithEntries.props.colorGroups[0]}
+        onUpdate={() => null}
+        onDelete={() => null}
+        editing
+      />);
+    expect(component.find('.delete-color-group-button')).to.have.length(1);
   });
 
-  describe('SingleColorGroupSettings subcomponents', () => {
-    let singleColorGroupEditing, singleColorGroupNotEditing;
-    beforeAll(() => {
-      singleColorGroupEditing = (
-        componentEditing.find('SingleColorGroupSettings').first().shallow()
-      );
-      singleColorGroupNotEditing = (
-        componentNotEditing.find('SingleColorGroupSettings').first().shallow()
-      );
-    });
-
-    it('contains a maxValue field', () => {
-      expect(singleColorGroupEditing.find('Input.color-group-maxValue-field'))
-        .to.have.length(1);
-    });
-
-    it('contains a minValue field', () => {
-      expect(singleColorGroupEditing.find('Input.color-group-minValue-field'))
-        .to.have.length(1);
-    });
-
-    it('contains a color field', () => {
-      expect(singleColorGroupEditing.find('Input.color-group-color-field'))
-        .to.have.length(1);
-    });
-
-    it('contains a "Delete color group" button if editing', () => {
-      expect(singleColorGroupEditing.find('Button.delete-color-group-button'))
-        .to.have.length(1);
-    });
-
-    it('does not contain a "Delete color group" button if not editing', () => {
-      expect(
-        singleColorGroupNotEditing.find('Button.delete-color-group-button'))
-        .to.have.length(0);
-    });
-
-    it('renders all fields as disabled if not editing', () => {
-      ['minValue', 'maxValue', 'color'].forEach(fieldName => {
-        let element = singleColorGroupNotEditing.find(
-          'Input.color-group-' + fieldName + '-field'
-        );
-        expect(element).to.have.property('node').and.to.have.property('props');
-        expect(element.node.props.disabled).to.be.ok;
-      });
-    });
-
-    it('renders all fields as not disabled if editing', () => {
-      ['minValue', 'maxValue', 'color'].forEach(fieldName => {
-        let element = singleColorGroupEditing.find(
-          'Input.color-group-' + fieldName + '-field'
-        );
-        expect(element).to.have.property('node').and.to.have.property('props');
-        expect(element.node.props.disabled).to.not.be.ok;
-      });
-    });
+  it('does not render a "delete color group" button if not editing', () => {
+    const component = shallow(
+      <SingleColorGroupSettings
+        colorGroup={MoodWithEntries.props.colorGroups[0]}
+        onUpdate={() => null}
+        onDelete={() => null}
+      />);
+    expect(component.find('.delete-color-group-button')).to.have.length(0);
   });
 
-  describe('actions', () => {
-    beforeEach(() => {
-      cbAction = null;
-      cbParams = null;
+  describe('callbacks', () => {
+    const onUpdate = jest.fn();
+    const onDelete = jest.fn();
+    const component = mount(
+      <SingleColorGroupSettings
+        colorGroup={MoodWithEntries.props.colorGroups[0]}
+        onUpdate={onUpdate}
+        onDelete={onDelete}
+        editing
+      />);
+
+    it('calls onUpdate when changing text field values', () => {
+      const minValueField = component.find('.color-group-minValue-field').first();
+      const maxValueField = component.find('.color-group-maxValue-field').first();
+      const colorField = component.find('.color-group-color-field').first();
+      minValueField.simulate('change');
+      maxValueField.simulate('change');
+      colorField.simulate('change');
+      expect(onUpdate.mock.calls).to.have.length(3);
+      expect(onUpdate.mock.calls[0][0], 'must call onUpdate from minValue-field')
+        .to.have.property('minValue').and.to.be.a('string');
+      expect(onUpdate.mock.calls[1][0], 'must call onUpdate from maxValue-field')
+        .to.have.property('maxValue').and.to.be.a('string');
+      expect(onUpdate.mock.calls[2][0], 'must call onUpdate from color-field')
+        .to.have.property('color').and.to.be.a('string');
     });
 
-    it('sends an "update form element" action when changing maxValue', () => {
-      let field = componentMountedEditing
-        .find('.color-group-maxValue-field > input').at(0);
-      field.node.value = '13';
-      field.simulate('change', field);
-      expect(cbAction).to.equal('update form element');
-      expect(cbParams).to.have.property('formId', 'metric-settings-form-1');
-      expect(cbParams).to.have.property('name', 'colorGroups');
-      expect(cbParams).to.have.property('value');
-      expect(cbParams.value).to.be.a('array').and.to.have.length(4);
-      expect(cbParams.value[0]).to.have.property('maxValue', '13');
+    it('calls onDelete when clicking delete button', () => {
+      const deleteButton = component.find('.delete-color-group-button').first();
+      deleteButton.simulate('click');
+      expect(onDelete.mock.calls).to.have.length(1);
     });
+  });
+});
 
-    it('sends an "update form element" action when changing minValue', () => {
-      let field = componentMountedEditing
-        .find('.color-group-minValue-field > input').at(0);
-      field.node.value = '13';
-      field.simulate('change', field);
-      expect(cbAction).to.equal('update form element');
-      expect(cbParams).to.have.property('formId', 'metric-settings-form-1');
-      expect(cbParams).to.have.property('name', 'colorGroups');
-      expect(cbParams).to.have.property('value');
-      expect(cbParams.value).to.be.a('array').and.to.have.length(4);
-      expect(cbParams.value[0]).to.have.property('minValue', '13');
-    });
+describe('ColorGroupsSettings', () => {
+  it('renders 4 SingleColorGroupSettings', () => {
+    const component = shallow(
+      <ColorGroupsSettings colorGroups={MoodWithEntries.props.colorGroups} editing />);
+    expect(component.find('SingleColorGroupSettings')).to.have.length(4);
+  });
 
-    it('sends an "update form element" action when changing color', () => {
-      let field = componentMountedEditing
-        .find('.color-group-color-field > input').at(0);
-      field.node.value = 'teal';
-      field.simulate('change', field);
-      expect(cbAction).to.equal('update form element');
-      expect(cbParams).to.have.property('formId', 'metric-settings-form-1');
-      expect(cbParams).to.have.property('name', 'colorGroups');
-      expect(cbParams).to.have.property('value');
-      expect(cbParams.value).to.be.a('array').and.to.have.length(4);
-      expect(cbParams.value[0]).to.have.property('color', 'teal');
-    });
+  it('renders a "new color group" button if editing', () => {
+    const component = shallow(
+      <ColorGroupsSettings
+        colorGroups={MoodWithEntries.props.colorGroups} onUpdate={() => null} editing
+      />);
+    expect(component.find('.add-color-group-button')).to.have.length(1);
+  });
 
-    it('sends "update form element" action when adding color group', () => {
-      componentMountedEditing.find('.add-color-group-button').first()
-        .simulate('click');
-      expect(cbAction).to.equal('update form element');
-      expect(cbParams).to.have.property('name', 'colorGroups');
-      expect(cbParams).to.have.property('value')
-        .and.to.have.length(5);
-    });
+  it('does not render a "new color group" button if not editing', () => {
+    const component = shallow(
+      <ColorGroupsSettings
+        colorGroups={MoodWithEntries.props.colorGroups} onUpdate={() => null}
+      />);
+    expect(component.find('.add-color-group-button')).to.have.length(0);
+  });
 
-    it('sends "update form element" action when deleting color group', () => {
-      componentMountedEditing.find('.delete-color-group-button').first()
-        .simulate('click');
-      expect(cbAction).to.equal('update form element');
-      expect(cbParams).to.have.property('name', 'colorGroups');
-      expect(cbParams).to.have.property('value')
-        .and.to.have.length(3);
-    });
+  it('calls onChange with {colorGroups: ...} as argument when changing a value', () => {
+    const onUpdate = jest.fn();
+    const component = mount(<ColorGroupsSettings
+      colorGroups={MoodWithEntries.props.colorGroups}
+      onUpdate={onUpdate}
+      editing
+    />);
+    component.find('.color-group-minValue-field').first().simulate('change');
+    expect(onUpdate.mock.calls).to.have.length(1);
+    expect(onUpdate.mock.calls[0][0]).to.have.property('colorGroups').and.to.have.length(4);
+  });
+
+  it('calls onChange with {colorGroups: ...} as argument when deleting a colorGroup', () => {
+    const onUpdate = jest.fn();
+    const component = mount(<ColorGroupsSettings
+      colorGroups={MoodWithEntries.props.colorGroups}
+      onUpdate={onUpdate}
+      editing
+    />);
+    component.find('.delete-color-group-button').first().simulate('click');
+    expect(onUpdate.mock.calls).to.have.length(1);
+    expect(onUpdate.mock.calls[0][0]).to.have.property('colorGroups').and.to.have.length(3);
+  });
+
+  it('calls onChange with {colorGroups: ...} as argument when adding a colorGroup', () => {
+    const onUpdate = jest.fn();
+    const component = mount(<ColorGroupsSettings
+      colorGroups={MoodWithEntries.props.colorGroups}
+      onUpdate={onUpdate}
+      editing
+    />);
+    component.find('.add-color-group-button').first().simulate('click');
+    expect(onUpdate.mock.calls).to.have.length(1);
+    expect(onUpdate.mock.calls[0][0]).to.have.property('colorGroups').and.to.have.length(5);
   });
 });
