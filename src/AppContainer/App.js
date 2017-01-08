48204099a68f2e87906f7e55b792fce8df567488
Nino Annighöfer <nino@ninoan.com>
2017-01-08 15:48:02 +0100

    Add props validation to App container

--- src/AppContainer/App.js
+++ src/AppContainer/App.js
@@ -9,23 +9,18 @@ import LoginScreen from '../components/LoginScreen';
 import { beginSyncData, beginCheckLogin } from '../actions';
 import { connect } from 'react-redux';
 import Modal from '../Modal';
-// import ActivityList from '../ActivityIndicator';
+import ActivityIndicator from '../ActivityIndicator';
+import { stateShapes } from '../types';
 
 export class App extends React.Component {
-  propTypes = {
-    dispatch: React.PropTypes.func.isRequired,
-    metrics: React.PropTypes.object.isRequired,
-    authentication: React.PropTypes.object.isRequired,
-  };
-
   componentDidMount() {
     const { dispatch, metrics, authentication } = this.props;
     const { isAuthenticated, isAuthenticating } = authentication;
     const { isSynced, isSyncing } = metrics;
-    if (!isSynced && isAuthenticated) {
+    if (!isSynced && !isSyncing && isAuthenticated) {
       dispatch(beginSyncData());
     }
-    if (!isAuthenticated && !isAuthenticating && !authentication.hasError) {
+    if (!isAuthenticated && !isAuthenticating && !authentication.error) {
       dispatch(beginCheckLogin());
     }
   }
@@ -35,41 +30,46 @@ export class App extends React.Component {
     if (!metrics.isSynced
       && authentication.isAuthenticated
       && !metrics.isSyncing
-      && !metrics.hasError) {
+      && !metrics.error) {
       dispatch(beginSyncData());
     }
   }
 
   render() {
     const { metrics, authentication } = this.props;
-    const { isAuthenticated, isAuthenticating } = authentication;
-    const { isSyncing, isSynced, items } = metrics;
+    const { isAuthenticated } = authentication;
+    const { items } = metrics;
     let child;
 
     if (!isAuthenticated) {
-      child = authentication.hasError ? <LoginScreen /> : <LoadingScreen />;
+      child = authentication.error ? <LoginScreen /> : <LoadingScreen />;
+    } else if (items) {
+      child = <MainUI />;
+    } else if (metrics.error) {
+      child = (<ErrorMessage>Could not load data ...</ErrorMessage>);
     } else {
-      if (items) {
-        child = <MainUI />;
-      } else if (metrics.hasError) {
-        child = (<ErrorMessage>Could not load data ...</ErrorMessage>);
-      } else {
-        child = <LoadingScreen />;
-      }
+      child = <LoadingScreen />;
     }
 
     return (
-      <div id='app-root'>
+      <div id="app-root">
         <AppHeader />
         {child}
         <AppFooter />
+        <ActivityIndicator />
         <Modal />
       </div>
     );
   }
 }
 
-const stateToProps = (state) => ({
+App.propTypes = {
+  dispatch: React.PropTypes.func.isRequired,
+  metrics: stateShapes.metrics,
+  authentication: stateShapes.authentication,
+};
+
+const stateToProps = state => ({
   authentication: state.authentication,
   metrics: state.metrics,
 });
