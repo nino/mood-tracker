c7dcf28b819a9d35dda79f94304e461601e6e5aa
Nino Annighöfer <nino@ninoan.com>
2017-01-08 11:41:04 +0100

    Simplify button-row metric-entry component

---
+++ src/DataEntry/ButtonRow.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from '../components/Button';
+
+const ButtonRow = ({ values, onClick }) => (
+  <div className="metric-entry-button-row">
+    {values.map(value => (
+      <Button key={value} onClick={() => onClick(value)}>
+        {value}
+      </Button>
+    ))}
+  </div>
+);
+
+ButtonRow.propTypes = {
+  values: React.PropTypes.arrayOf(React.PropTypes.number).isRequired,
+  onClick: React.PropTypes.func.isRequired,
+};
+
+export default ButtonRow;
---
+++ src/DataEntry/ButtonRow.test.js
@@ -0,0 +1,22 @@
+/* eslint-env jest */
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+
+import ButtonRow from './ButtonRow';
+
+describe('Metric-entry button-row', () => {
+  it('renders one button per passed value', () => {
+    const component = shallow(<ButtonRow values={[1, 2, 3]} onClick={jest.fn()} />);
+    expect(component.find('Button')).to.have.length(3);
+  });
+
+  it('calls onClick with the value of the clicked button', () => {
+    const onClick = jest.fn();
+    const component = mount(<ButtonRow values={[1, 2, 3]} onClick={onClick} />);
+    component.find('button').first().simulate('click');
+    expect(onClick.mock.calls).to.have.length(1);
+    expect(onClick.mock.calls[0][0]).to.equal(1);
+  });
+});
