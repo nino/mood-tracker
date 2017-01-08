89f560d068cb662730b45b1931b8d7eb93da1d8a
Nino Annighöfer <nino@ninoan.com>
2017-01-08 15:29:27 +0100

    Rewrite data entry container with Redux actions

--- src/DataEntry/DataEntryContainer.test.js
+++ src/DataEntry/DataEntryContainer.test.js
@@ -1,17 +1,25 @@
+/* eslint-env jest */
+/* eslint-disable no-unused-expressions */
 import React from 'react';
-import {shallow, render, mount} from 'enzyme';
-import {expect} from 'chai';
-import DataEntryContainer from './DataEntryContainer';
-import SampleMetricsWithEntries from '../../test/SampleMetricsWithEntries';
-import SampleMetricsWithoutEntries from '../../test/SampleMetricsWithEntries';
-import SampleMetricsCorruptData from '../../test/SampleMetricsCorruptData';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import {
+  MoodWithEntries,
+  BurnsWithoutEntries,
+} from '../../test/SampleMetrics';
+
+import { DataEntryContainer } from './DataEntryContainer';
+import MetricEntryContainer from './MetricEntryContainer';
 
 describe('DataEntryContainer', () => {
-  it('renders 2 MetricEntryContainers', () => {
-    const callback = () => null;
+  it('renders `MetricEntryContainer`s', () => {
     const component = shallow(
-      <DataEntryContainer metrics={SampleMetricsWithEntries} onAction={callback} />
-    );
-    expect(component.find('MetricEntryContainer')).to.have.length(2);
+      <DataEntryContainer metrics={[MoodWithEntries, BurnsWithoutEntries]} />);
+    expect(component.find(MetricEntryContainer)).to.have.length(2);
+  });
+
+  it('renders a message if no metrics', () => {
+    const component = shallow(<DataEntryContainer metrics={[]} />);
+    expect(component.text()).to.include('yet');
   });
 });
