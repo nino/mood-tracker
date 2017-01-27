2008ab84fbd14cde4acff9f44c3309f251c2edf1
Nino Annighöfer <nino@ninoan.com>
2017-01-27 15:08:46 +0100

    Eject react-app and add flow
    To get more fine-grained control over the build-system, I ran `npm run eject`, which opens up the create-react-app starter kit.
    Next, I added dependencies for flow and generated libdefs.
    
    Unfortunately, there are no libdefs for BlueprintJS.
    
    I also updated a lot of the scripts in package.json because I was running into problems. Hopefully all the dependencies should work now.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
+  "parser": "babel-eslint",
   "extends": "airbnb",
   "installedESLint": true,
   "plugins": [
+    "babel",
     "react",
     "jsx-a11y",
-    "import"
+    "import",
+    "flowtype"
   ],
   "rules": {
+    "babel/flow-object-type": 1,
+    "react/sort-comp": "off",
     "react/jsx-filename-extension": "off",
     "import/no-extraneous-dependencies": [
       "error",
       {"devDependencies": ["**/*.test.js", "**/*.spec.js"]}
     ],
-    "import/no-named-as-default": "off"
+    "import/no-named-as-default": "off",
+    "max-len": "off"
   },
 };
