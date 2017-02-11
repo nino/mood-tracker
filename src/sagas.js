6a153eaabff4a84ec29396434a22234c703100e7
Nino Annighöfer <nino@ninoan.com>
2017-02-11 11:24:42 +0100

    Annotate sagas with types and improve tests

--- src/sagas.js
+++ src/sagas.js
@@ -1,9 +1,9 @@
 /* @flow */
-/* global window localStorage */
-import { put, select, call } from 'redux-saga/effects';
+/* global window localStorage Generator */
 import { takeLatest, takeEvery } from 'redux-saga';
 import Dropbox from 'dropbox';
 import queryString from 'query-string';
+import { put, select, call } from './effect-generators';
 import { DATA_FILE_PATH } from './constants';
 import {
   successCheckLogin,
@@ -38,8 +38,8 @@ import {
   isValidMetricsArray,
 } from './lib';
 
-export function* syncData() {
-  const authentication = yield select(getAuthentication);
+export function* syncData(): Generator<any, any, any> {
+  const authentication: TAuthenticationState = yield* select(getAuthentication);
   const accessToken = authentication.accessToken;
   /*
    * We download the data from Dropbox,
@@ -48,105 +48,109 @@ export function* syncData() {
    * - For entries, we just merge the arrays, keeping them sorted by date.
    */
   if (!accessToken) {
-    yield put(errorSyncData('Not authenticated'));
+    yield* put(errorSyncData('Not authenticated'));
   } else {
-    yield put(requestRestoreCache());
-    const metricsFromStore = yield select(getMetricsItems);
+    yield* put(requestRestoreCache());
+    const metricsFromStore = yield* select(getMetricsItems);
     const localMetrics = metricsFromStore || [];
     const dbx = new Dropbox({ accessToken });
-    const apiResponse = yield call(downloadFileAsJSON, dbx, DATA_FILE_PATH);
-    if (!apiResponse.ok && apiResponse.error !== 'Error: File not found') {
-      localStorage.metrics = JSON.stringify(localMetrics);
-      yield put(errorSyncData(apiResponse.error));
+    const apiResponse: { ok: false, error: string } | { ok: true, data: any[] } = yield* call(downloadFileAsJSON, dbx, DATA_FILE_PATH);
+    if (apiResponse.ok === false && apiResponse.error !== 'Error: File not found') {
+      localStorage.setItem('metrics', JSON.stringify(localMetrics));
+      yield* put(errorSyncData(apiResponse.error));
     } else {
       const remoteMetrics = apiResponse.data ? apiResponse.data : [];
       const mergedMetrics = mergeMetrics(localMetrics.concat(remoteMetrics));
-      localStorage.metrics = JSON.stringify(mergedMetrics);
-      const uploadResponse = yield call(
+      localStorage.setItem('metrics', JSON.stringify(mergedMetrics));
+      const uploadResponse: { ok: true} | { ok: false, error: string } = yield* call(
         uploadAsJSON,
         dbx,
         DATA_FILE_PATH,
         mergedMetrics,
       );
       if (!uploadResponse.ok) {
-        yield put(errorSyncData('Upload error'));
+        yield* put(errorSyncData('Upload error'));
         return;
       }
 
-      yield put(successSyncData(mergedMetrics, (new Date()).getTime()));
+      yield* put(successSyncData(mergedMetrics, (new Date()).getTime()));
     }
   }
 }
 
-export function* restoreCache() {
-  const metricsItems = yield select(getMetricsItems);
-  if (metricsItems === null && typeof localStorage.metrics === 'string') {
-    const restoredMetrics = JSON.parse(localStorage.metrics);
-    if (restoredMetrics instanceof Array && isValidMetricsArray(restoredMetrics)) {
-      yield put(successRestoreCache(restoredMetrics));
-      return;
+export function* restoreCache(): Generator<any, any, any> {
+  const metricsItems: ?TMetric[] = yield* select(getMetricsItems);
+  try {
+    const metricsCache = localStorage.getItem('metrics');
+    if (metricsItems == null && metricsCache != null) {
+      const restoredMetrics = JSON.parse(metricsCache);
+      if (restoredMetrics instanceof Array && isValidMetricsArray(restoredMetrics)) {
+        yield* put(successRestoreCache(restoredMetrics));
+        return;
+      }
     }
+    yield* put(errorRestoreCache('Either no cached data present or data already loaded'));
+  } catch (error) {
+    yield* put(errorRestoreCache('Could not parse localStorage metrics'));
   }
-
-  yield put(errorRestoreCache('Either no cached data present or data already loaded'));
 }
 
-export function* checkLogin() {
+export function* checkLogin(): Generator<any, any, any> {
   // Find access token:
   // 1. Try local storage.
-  const localToken = localStorage.accessToken;
+  const localToken = localStorage.getItem('accessToken');
   // 2. Try URL hash -- URL hash overrules local storage.
-  const hashToken = queryString.parse(window.location.hash).access_token;
+  const hashToken: ?string = queryString.parse(window.location.hash).access_token;
   const token = hashToken || localToken;
   // 3.1. If we have a token, succeed.
   if (token) {
-    localStorage.accessToken = token;
+    localStorage.setItem('accessToken', token);
     window.location.hash = '';
     const lastAuthenticated = (new Date()).getTime();
-    yield put(successCheckLogin(token, lastAuthenticated));
+    yield* put(successCheckLogin(token, lastAuthenticated));
   } else { // 3.2. If we don't have a token, fail.
-    yield put(errorCheckLogin('No token found'));
+    yield* put(errorCheckLogin('No token found'));
   }
 }
 
-export function* executeConfirmModal() {
-  const modals = yield select(getModals);
+export function* executeConfirmModal(): Generator<any, any, any> {
+  const modals = yield* select(getModals);
   if (modals.length < 1) {
-    yield put(errorConfirmModal());
+    yield* put(errorConfirmModal());
   } else {
-    yield put(modals[0].actions.confirm.action);
-    yield put(successConfirmModal());
+    yield* put(modals[0].actions.confirm.action);
+    yield* put(successConfirmModal());
   }
 }
 
-export function* executeCancelModal() {
-  const modals = yield select(getModals);
+export function* executeCancelModal(): Generator<any, any, any> {
+  const modals = yield* select(getModals);
   if (modals.length < 1) {
-    yield put(errorCancelModal());
+    yield* put(errorCancelModal());
   } else {
-    yield put(modals[0].actions.cancel.action);
-    yield put(successCancelModal());
+    yield* put(modals[0].actions.cancel.action);
+    yield* put(successCancelModal());
   }
 }
 
-export function* executeLogout() {
-  delete localStorage.accessToken;
-  yield put(successLogout());
+export function* executeLogout(): Generator<any, any, any> {
+  localStorage.removeItem('accessToken');
+  yield* put(successLogout());
 }
 
-export function* executeSyncData() {
+export function* executeSyncData(): Generator<any, any, any> {
   yield put(beginSyncData());
 }
 
-export function* watcherSaga() {
-  yield takeEvery('LOG_METRIC', executeSyncData);
-  yield takeEvery('UPDATE_METRIC', executeSyncData);
-  yield takeEvery('DELETE_METRIC', executeSyncData);
-  yield takeEvery('REORDER_METRICS', executeSyncData);
-  yield takeLatest('BEGIN_CHECK_LOGIN', checkLogin);
-  yield takeLatest('BEGIN_SYNC_DATA', syncData);
-  yield takeEvery('REQUEST_CONFIRM_MODAL', executeConfirmModal);
-  yield takeEvery('REQUEST_CANCEL_MODAL', executeCancelModal);
-  yield takeEvery('REQUEST_LOGOUT', executeLogout);
+export function* watcherSaga(): Generator<any, any, any> {
+  yield* takeEvery('LOG_METRIC', executeSyncData);
+  yield* takeEvery('SUCCESS_UPDATE_METRIC', executeSyncData);
+  yield* takeEvery('DELETE_METRIC', executeSyncData);
+  yield* takeEvery('REORDER_METRICS', executeSyncData);
+  yield* takeLatest('BEGIN_CHECK_LOGIN', checkLogin);
+  yield* takeLatest('BEGIN_SYNC_DATA', syncData);
+  yield* takeEvery('REQUEST_CONFIRM_MODAL', executeConfirmModal);
+  yield* takeEvery('REQUEST_CANCEL_MODAL', executeCancelModal);
+  yield* takeEvery('REQUEST_LOGOUT', executeLogout);
   yield takeEvery('REQUEST_RESTORE_CACHE', restoreCache);
 }
