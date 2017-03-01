a4c002cd89d0427175e9d5abd86d3e4c25dade96
Nino Annighöfer <nino@ninoan.com>
2017-03-01 15:00:44 +0100

    Make App tests more type-safe

--- src/AppContainer/App.test.js
+++ src/AppContainer/App.test.js
@@ -13,6 +13,8 @@ import {
   settingsSubStates,
 } from '../../test/SampleApplicationStates';
 
+import type { TApplicationState } from '../types';
+
 import ConnectedApp, { App } from './App';
 import LoginScreen from '../components/LoginScreen';
 
@@ -37,29 +39,41 @@ describe('App', () => {
       metricsSubStates.notSyncingWithData,
     ];
     metricsOptions.forEach((metricsOption) => {
-      const store = mockStore({
+      const state: TApplicationState = {
         metrics: metricsOption,
         settings: settingsSubStates.notEditing,
         authentication: authSubStates.authenticated,
         modals: [],
-      });
+        charts: [],
+      };
+      const store = mockStore(state);
       store.dispatch = jest.fn();
 
       mount(<Provider store={store}><ConnectedApp /></Provider>);
 
-      expect(store.dispatch.mock.calls).to.have.length(1);
-      expect(store.dispatch.mock.calls[0]).to.have.length(1);
-      expect(store.dispatch.mock.calls[0][0]).to.have.property('type', 'BEGIN_SYNC_DATA');
+      const numCalls = store.dispatch.mock.calls.length;
+      expect(numCalls).to.be.a('number').that.is.above(0);
+      if (numCalls === 1) {
+        expect(store.dispatch.mock.calls[0], JSON.stringify(store.dispatch.mock.calls)).to.have.length(1); // TODO definitely not elegant
+        expect(store.dispatch.mock.calls[0][0]).to.have.property('type', 'BEGIN_SYNC_DATA');
+      } else if (numCalls === 2) {
+        expect(store.dispatch.mock.calls[1], JSON.stringify(store.dispatch.mock.calls)).to.have.length(1); // TODO definitely not elegant
+        expect(store.dispatch.mock.calls[1][0]).to.have.property('type', 'BEGIN_SYNC_DATA');
+      } else {
+        expect(false, 'there is an error').to.be.ok;
+      }
     });
   });
 
   it('dispatches "begin check login" if not authenticated and not error', () => {
-    const store = mockStore({
+    const state: TApplicationState = {
       metrics: metricsSubStates.notSyncingNoData,
       authentication: authSubStates.notAuthenicatedNotAuthenticating,
       modals: [],
       settings: settingsSubStates.notEditing,
-    });
+      charts: [],
+    };
+    const store = mockStore(state);
     store.dispatch = jest.fn();
 
     mount(<Provider store={store}><ConnectedApp /></Provider>);
