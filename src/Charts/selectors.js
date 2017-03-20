e2686c9c31bf736ad18ed82999ac50e55f97428c
Nino Annighöfer <nino@ninoan.com>
2017-03-20 17:43:08 +0100

    Make charts update when metrics change
    When adding a data point for a metric, its chart's dateRange gets extended.
    
    Charts are recreated when metrics are updated or deleted.

--- src/Charts/selectors.js
+++ src/Charts/selectors.js
@@ -1,11 +1,18 @@
 /* @flow */
-import type { TApplicationState, TChart } from '../types';
+import {
+  findIndex,
+} from 'lodash';
+import type {
+  TApplicationState,
+  TChart,
+} from '../types';
+import { chartIndexFromMetricId } from './lib';
 
 export const getCharts = (state: TApplicationState) => state.charts;
 
 export const getChart = (state: TApplicationState, chartId: number) => {
   const charts = getCharts(state);
-  const index = charts.findIndex((chart: TChart) => chart.id === chartId);
+  const index = findIndex(charts, (chart: TChart) => chart.id === chartId);
   if (index === -1) {
     return null;
   }
@@ -13,3 +20,13 @@ export const getChart = (state: TApplicationState, chartId: number) => {
 };
 
 export const getChartAnimation = (state: TApplicationState, chartId: number) => state.charts[chartId].animation;
+
+export const getChartByMetricId = (state: TApplicationState, metricId: number) => {
+  const charts = getCharts(state);
+  const idx = chartIndexFromMetricId(state.charts, metricId);
+  if (idx == null) {
+    return null;
+  }
+  return charts[idx];
+};
+
