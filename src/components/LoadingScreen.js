a1ebf71963ba03e0af12d0228e83b14a77f574f2
Nino Annighöfer <nino@ninoan.com>
2017-04-02 21:43:58 +0200

    Delete everything except the build

--- src/components/LoadingScreen.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-import React from 'react';
-
-const LoadingScreen = () => (
-  <div className="hero-wrapper">Loading ...</div>
-);
-
-export default LoadingScreen;
--- src/selectors.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-import type { TApplicationState } from './types';
-
-export const getAuthentication = (state: TApplicationState) => state.authentication;
-export const getMetrics = (state: TApplicationState) => state.metrics;
-export const getMetricsItems = (state: TApplicationState) => state.metrics.items;
-export const getSettings = (state: TApplicationState) => state.settings;
-export const getModals = (state: TApplicationState) => state.modals;
